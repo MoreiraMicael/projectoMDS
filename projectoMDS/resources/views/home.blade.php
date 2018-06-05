198f2540ca28c18d8ff08ab81a2b6d9e2d768295
Moreira <moreira_micael@hotmail.com>
2018-06-05 17:10:12 +0100

    autorizações na pagina inicial para ver links por roles roles: admin, ps, utente

--- projectoMDS/resources/views/home.blade.php
+++ projectoMDS/resources/views/home.blade.php
@@ -2,22 +2,47 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="card-body">
+          @if (session('status'))
+          <div class="alert alert-success">
+            {{ session('status') }}
+          </div>
+          @endif
 
-                    You are logged in!
-                </div>
-            </div>
+          You are logged in!
+
+
+          <div class="alert alert-failure">
+            <a href="">Bem-vindo <?php echo Auth::user()->name; ?> </a>
+            <br>
+
+            @if(Auth::user()->role == 'admin')
+            <a href="">Admin</a>
+            @endif
+            <br>
+
+            @if(Auth::user()->role == 'ps')
+            <a href="">Profissional de Saude</a>
+            @endif
+            <br>
+
+            @if(Auth::user()->role == 'utente')
+            <a href="">Utente</a>
+            @endif
+            <br>
+
+          </div>
+
+
+
         </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
